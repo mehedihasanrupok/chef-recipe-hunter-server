66ae1fd3bbe1df0bfcb8567900d6a36cd039bca1
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-05-04 20:33:01 +0600

    corslibary added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-
+const chefs = require('./data/chef.json');
 
 
 app.get('/', (req, res) => {
@@ -16,11 +16,6 @@ app.get('/chefs', (req, res) => {
     res.send(chefs);
 });
 
-app.get('/chefs/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedChef = chefs.find(n => n._id === id);
-    res.send(selectedChef);
-});
 
 app.listen(port, () => {
   console.log(`This app listening on port ${port}`)
