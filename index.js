7de35a2c6622434eae8957b31d8ba679f6bf50ab
mehedihasanrupok <hasanmehedi4114@gmail.com>
2023-05-04 22:41:18 +0600

    fort

--- index.js
+++ index.js
@@ -12,9 +12,16 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/chefs', (req, res) => {
-    res.send(chefs);
+// app.get('/chefs', (req, res) => {
+//     res.send(chefs);
+// });
+
+app.get('/chefs/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedChef = chefs.find(n => n._id === id);
+  res.send(selectedChef);
 });
+
 
 
 app.listen(port, () => {
